5f5cb42e524a32605c3817049fdc1392fbb7b5ae
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-03 21:02:17 -0300

    feat: :tada: Endpoint para adicionar participante em um bolão

--- src/routes/poll.ts
+++ src/routes/poll.ts
@@ -4,6 +4,8 @@ import ShortUniqueId from "short-unique-id";
 
 import { prisma } from "../lib/prisma";
 
+import { authenticate } from "../plugins/authenticate";
+
 export async function pollRoutes(fastify: FastifyInstance) {
 	fastify.get("/polls/count", async () => {
 		const count = await prisma.poll.count();
@@ -14,11 +16,11 @@ export async function pollRoutes(fastify: FastifyInstance) {
 	});
 
 	fastify.post("/polls", async (request, reply) => {
-		const createPoolBody = z.object({
+		const createPollBody = z.object({
 			title: z.string()
 		});
 
-		const { title } = createPoolBody.parse(request.body);
+		const { title } = createPollBody.parse(request.body);
 
 		const generate = new ShortUniqueId({
 			length: 6
@@ -54,4 +56,63 @@ export async function pollRoutes(fastify: FastifyInstance) {
 			code
 		});
 	});
+
+	fastify.post(
+		"/polls/join",
+		{
+			onRequest: [authenticate]
+		},
+		async (request, reply) => {
+			const joinPollBody = z.object({
+				code: z.string()
+			});
+
+			const { code } = joinPollBody.parse(request.body);
+
+			const poll = await prisma.poll.findUnique({
+				where: {
+					code
+				},
+				include: {
+					participants: {
+						where: {
+							userId: request.user.sub
+						}
+					}
+				}
+			});
+
+			if (!poll) {
+				return reply.status(404).send({
+					message: "Poll not found."
+				});
+			}
+
+			if (poll.participants.length > 0) {
+				return reply.status(400).send({
+					message: "You already join this poll."
+				});
+			}
+
+			if (!poll.ownerId) {
+				await prisma.poll.update({
+					where: {
+						id: poll.id
+					},
+					data: {
+						ownerId: request.user.sub
+					}
+				});
+			}
+
+			await prisma.participant.create({
+				data: {
+					pollId: poll.id,
+					userId: request.user.sub
+				}
+			});
+
+			return reply.status(201).send();
+		}
+	);
 }
